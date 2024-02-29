06875e96e24f58742c0518f2165f43399dcb5886
Homeomorphistic <chmiela.bartosz@gmail.com>
2024-02-29 20:10:05 +0100

    compounding returns done, working on splitting returns.

--- risk-tolerance/ETL/ETL.R
+++ risk-tolerance/ETL/ETL.R
@@ -41,28 +41,114 @@ returns.from.prices <- function(prices) {
   return(returns)
 }
 
-format.to.percentage <- function(percent, n=length(percent)) {
-  #' Formats percents to standard way of X.XX%
+format.to.percentage <- function(returns, n=length(returns)) {
+  #' Formats returns to standard way of X.XX%
   #'
-  #' @param numeric. A numeric vector o percents.
+  #' @param numeric. A numeric vector o returns.
   #' @param n numeric. A numeric scalar indicating number of first terms to return.
   #'  If negative, return last n terms.
 
   # If n is negative, return last n terms.
   if(n < 0){ print(n)
-    percent <- tail(percent, -n)}
+    returns <- tail(returns, -n)}
   else
-    percent <- head(percent, n)
+    returns <- head(returns, n)
 
   # sprintf will drop vector names, so save them for later.
-  dates <- names(percent)
+  dates <- names(returns)
   # Format values, round to 2nd decimal place.
-  percent <- sprintf("%1.2f%%", 100*percent)
-  names(percent) <- dates
+  returns <- sprintf("%1.2f%%", 100*returns)
+  names(returns) <- dates
 
-  return(percent)
+  return(returns)
+}
+
+.coumpound.returns <- function(returns, n) {
+  #' Compound returns into less but longer periods.
+  #'
+  #' @param returns numeric. A numeric vector of returns.
+  #' @n numeric. A scalar indicating how many periods
+  #'    to compound into one.
+  #' @return transformed_returns numeric. A numeric vector
+  #'    of compounded returns.
+
+  n_periods <- length(returns)
+  periods <- names(returns)
+  # Prepare target period names.
+  progression <- seq(n, n_periods, by=n)
+  target_periods <- periods[progression]
+
+  # Find number of periods with target frequency.
+  n_target <- n_periods / n
+  # Prepare grouping vector. Ex. 1,1,1,2,2,2,...,n_target,n_target,n_target
+  groups <- sort(rep(1:n_target, length.out=n_periods))
+  # Split returns into groups that will be compounded.
+  splitted <- split(1+returns, groups)
+  # Compound.
+  transformed_returns <- unlist(lapply(splitted, prod)) - 1
+  names(transformed_returns) <- target_periods
+
+  return(transformed_returns)
+}
+
+.split.returns <- function(returns, n) {
+  #' Split returns into more and smaller periods.
+  #'
+  #' @param returns numeric. A numeric vector of returns.
+  #' @n numeric. A scalar indicating how many periods
+  #'    to split one into.
+  #' @return transformed_returns numeric. A numeric vector
+  #'    of splitted returns.
+
+  periods <- names(returns)
+  start_date <- periods[1]
+  end_date <- periods[length(periods)]
+  # For each return, split it into n equal returns.
+  transformed_returns <- unlist(lapply(returns,
+                                       function(returns) rep((1+returns)^(1/n)-1, n)))
+  # Get sequence of dates for target returns.
+  periods_target <- seq(start_date, end_date, by=paste0(n, " months"))
+  names(transformed_returns) <- periods_target
+  # TODO finish dates.
+  return(transformed_returns)
+}
+
+transform.returns <- function(returns, freq=12, target_freq=1) {
+  #' Transform returns to match different frequency.
+  #'
+  #' Transform returns from current frequency to target frequency.
+  #' Frequency of 1 is a yearly frequency.
+  #'
+  #' @param returns numeric. A numeric vector of returns.
+  #' @param freq numeric.  Current frequency of returns.
+  #' @param target_freq numeric. Target frequency of returns.
+
+  # TODO throw errors if not compatible frequencies.
+  # Current number of periods with returns.
+  n_periods <- length(returns)
+  periods <- names(returns)
+
+  if(freq > target_freq){ # If you want to compound returns,
+    # How many periods are in target period?
+    # 1/freq is the length of period in terms of year, analogously 1/target,
+    # so 1/target / 1/freq = freq/target
+    n_prd_in_tgt <- freq / target_freq
+
+    transformed_returns <- .coumpound.returns(returns, n_prd_in_tgt)
+  }
+  else { # If you want to split returns into smaller chunks.
+    n_prd_in_tgt <- target_freq / freq
+
+  }
+
+  return(transformed_returns)
 }
 
 tbsp<-read.stooq.asset.price("./data/input/Poland/tbsp_m.csv")
 tbsp_returns <- returns.from.prices(tbsp)
-format.to.percentage(tbsp_returns, -2)
+tbsp_returns_yr <- transform.returns(tbsp_returns[c(-205,-206)])
+x<-.split.returns(tbsp_returns_yr, 12)
+
+d1 <- as.Date("2022-12-31")
+d2 <- as.Date("2023-12-31")
+seq(d1, d2, by="2 month")
