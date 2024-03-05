fe3aa689b8ed253b582ea127f15292cbf002ed02
Homeomorphistic <chmiela.bartosz@gmail.com>
2024-03-05 20:20:55 +0100

    Finished docstrings, prepared plotting, done with general ETL.

--- risk-tolerance/ETL/ETL.R
+++ risk-tolerance/ETL/ETL.R
@@ -63,54 +63,68 @@ format.to.percentage <- function(returns, n=length(returns)) {
   return(returns)
 }
 
-.coumpound.returns <- function(returns, n) {
-  #' Compound returns into less but longer periods.
+.compound.returns <- function(returns, n) {
+  #' Compound returns into less periods but longer ones.
   #'
-  #' @param returns numeric. A numeric vector of returns.
+  #' @param returns numeric. A numeric named vector of returns.
   #' @n numeric. A scalar indicating how many periods
   #'    to compound into one.
   #' @return transformed_returns numeric. A numeric vector
   #'    of compounded returns.
 
+  # How many periods there are? What are those periods?
   n_periods <- length(returns)
   periods <- names(returns)
-  # Prepare target period names.
+  # New longer periods are the subsequence of periods with start n and step of n.
   progression <- seq(n, n_periods, by=n)
-  target_periods <- periods[progression]
+  longer_periods <- periods[progression]
 
-  # Find number of periods with target frequency.
-  n_target <- n_periods / n
-  # Prepare grouping vector. Ex. 1,1,1,2,2,2,...,n_target,n_target,n_target
-  groups <- sort(rep(1:n_target, length.out=n_periods))
+  # How many new longer periods are in old ones?
+  n_longer <- n_periods / n
+  # Prepare grouping vector. It will group smaller periods into groups,
+  # which will be compounded. Ex. 1,1,1,2,2,2,...,n_longer,n_longer,n_longer
+  groups <- sort(rep(1:n_longer, length.out=n_periods))
   # Split returns into groups that will be compounded.
-  splitted <- split(1+returns, groups)
-  # Compound.
-  transformed_returns <- unlist(lapply(splitted, prod)) - 1
-  names(transformed_returns) <- target_periods
+  splitted <- split(1+returns, groups) # add 1 for later multiplication.
+  # Compound. For every group in list, take product of grouped returns.
+  transformed_returns <- unlist(lapply(splitted, prod)) - 1 # subtract 1 to get retuns.
+  names(transformed_returns) <- longer_periods
 
   return(transformed_returns)
 }
 
-.split.returns <- function(returns, n) {
-  #' Split returns into more and smaller periods.
+.split.returns <- function(returns, n, freq) {
+  #' Split returns into more but smaller periods.
   #'
-  #' @param returns numeric. A numeric vector of returns.
-  #' @n numeric. A scalar indicating how many periods
+  #' @param returns numeric. A numeric named vector of returns.
+  #' @param n numeric. A scalar indicating how many periods
   #'    to split one into.
+  #' @param freq numeric. A scalar indicating present frequency of returns.
   #' @return transformed_returns numeric. A numeric vector
   #'    of splitted returns.
 
+  # What are the periods? What is the starting date?
   periods <- names(returns)
   start_date <- as.Date(periods[1])
-  # Subtract one year.
-  start_date <- seq(start_date, length.out = 2, by="-1 year")[2]
+  # Subtract one period. To do that, create sequence of dates, from starting_date to a period ago.
+  start_date <- seq(start_date, length.out = 2, by=paste(-12/freq, "months"))[2]
+  # We need it, because dates represent returns after a period (for example a year) and
+  # if we want to split a year into smaller periods, we will need months (or anything else)
+  # before that date.
+
   end_date <- as.Date(periods[length(periods)])
+
   # For each return, split it into n equal returns.
   transformed_returns <- unlist(lapply(returns,
                                        function(returns) rep((1+returns)^(1/n)-1, n)))
-  # Get sequence of dates for target returns.
-  periods_target <- seq(start_date+1, end_date+1, by=paste0(12/n, " month"))-1 # TODO explain yourself
-  names(transformed_returns) <- periods_target[-1] # TODO explain
+  # Get sequence of dates for new smaller periods.
+  smaller_periods <- seq(start_date+1, end_date+1, by=paste0(12/n, " month"))-1
+  # We are adding one to date, to make it 1st of the next month. It's gives
+  # accurate sequence of dates (always first), while it has problems with last dates
+  # (for example sometimes skips 28th Feb to 2nd March)
+  # Then subtract one, to get back to last day of previous month.
+
+  names(transformed_returns) <- smaller_periods[-1] # First period is before the original period.
 
   return(transformed_returns)
 }
@@ -121,12 +135,11 @@ transform.returns <- function(returns, freq=12, target_freq=1) {
   #' Transform returns from current frequency to target frequency.
   #' Frequency of 1 is a yearly frequency.
   #'
-  #' @param returns numeric. A numeric vector of returns.
-  #' @param freq numeric.  Current frequency of returns.
-  #' @param target_freq numeric. Target frequency of returns.
+  #' @param returns numeric. A numeric named vector of returns.
+  #' @param freq numeric. A scalar indicating current frequency of returns.
+  #' @param target_freq numeric. A scalar indicating target frequency of returns.
 
-  # TODO throw errors if not compatible frequencies.
-  # Current number of periods with returns.
+  # How many periods? What are they?
   n_periods <- length(returns)
   periods <- names(returns)
 
@@ -140,20 +153,36 @@ transform.returns <- function(returns, freq=12, target_freq=1) {
   }
   else { # If you want to split returns into smaller chunks.
     n_prd_in_tgt <- target_freq / freq
-
+    transformed_returns <- .split.returns(returns, n_prd_in_tgt, freq)
   }
 
   return(transformed_returns)
 }
 
+plot.returns <- function(returns, title="Returns") {
+  #' Plot cumulative returns.
+  #'
+  #' @param returns numeric. A numeric named vector of returns.
+  #' @param title character. A string with plot title.
+
+  plot(as.Date(names(returns)), cumprod(1+returns),
+       type = "l", ylab = "Returns", xlab = "time", main = title)
+}
+
+add.plot.returns <- function(returns, colour="red") {
+  #' ADd cumulative returns to another plot.
+  #'
+  #' @param returns numeric. A numeric named vector of returns.
+
+  lines(as.Date(names(returns)), cumprod(1+returns), col = colour)
+}
+
 tbsp<-read.stooq.asset.price("./data/input/Poland/tbsp_m.csv")
 tbsp_returns <- returns.from.prices(tbsp)[c(-205,-206)]
-tbsp_returns_yr <- transform.returns(tbsp_returns)
-x<-.split.returns(tbsp_returns_yr, 12)
+tbsp_returns_qr <- transform.returns(tbsp_returns, 12, 4)
+tbsp_returns_hf <- transform.returns(tbsp_returns, 12, 2)
+tbsp_returns_yr <- transform.returns(tbsp_returns, 12, 1)
 
-plot(as.Date(names(tbsp_returns)),
-     cumprod(tbsp_returns+1),
-     type = "l",
-     xlim = c(as.Date("2007-01-31"), as.Date("2023-12-31"))
-)
-lines(as.Date(names(tbsp_returns)), cumprod(x+1), type = "l", col="red")
+plot.returns(tbsp_returns, "tbsp monthly")
+add.plot.returns(transform.returns(tbsp_returns, 12, 1), "red")
+add.plot.returns(transform.returns(tbsp_returns, 12, 4), "blue")
