c0cf344045e627d52716d28720f5fc44ca4d43cf
Homeomorphistic <chmiela.bartosz@gmail.com>
2024-04-06 18:03:14 +0200

    Updated polish CPI, started a quick polish real returns analysis.

--- risk-tolerance/analysis.R
+++ risk-tolerance/analysis.R
@@ -10,3 +10,13 @@ real.returns <- function(returns, cpi) {
 
   return( (1+returns) / (1+cpi) - 1)
 }
+
+cpi <- select.returns(pl_cpi_mom, "1991-07-31", "2024-02-29")
+real_tbsp <- real.returns(pl_tbsp_extended, cpi)
+pl_wig <- select.returns(pl_wig, "1991-07-31", "2024-02-29")
+real_wig <- real.returns(pl_wig, cpi)
+
+plot.returns(real_wig)
+add.plot.returns(real_tbsp)
+add.plot.returns(0.5*real_wig + 0.5*real_tbsp, col="blue")
+add.plot.returns(0.3*real_wig + 0.7*real_tbsp, col="green")
